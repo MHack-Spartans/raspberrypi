d8c6991e5f1455f18348dad58e5914f131aaebb0
Austin Evans <austinjevans@austins-mbp.local>
2018-10-13 14:10:27 -0400

    serial and mqtt

---
+++ serial.py
@@ -0,0 +1,11 @@
+import serial
+
+def activate_slot(slot_num):
+    ser.write(str(slot_num))
+
+ser = serial.Serial("/dev/ttyUSB0", 9600)
+ser.flushInput()
+
+while True:
+    num = input("Slot: ")
+    activate_slot(num)
