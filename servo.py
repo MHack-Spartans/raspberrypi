8502d5e1e7cc3e989c52438a6db72b4a7f131c80
Austin Evans <austinjevans@austins-mbp.local>
2018-10-13 03:08:41 -0400

    servo test file

---
+++ servo.py
@@ -0,0 +1,14 @@
+import pigpio
+import time
+
+SERVO_PIN = 37
+
+def servo_set_angle(angle):
+    width = float(angle) * 100.0 / 9.0 + 500 # Convert angle (degrees) to pulse width
+    pi.set_servo_pulsewidth(SERVO_PIN, width)
+
+while True:
+    servo_set_angle(0)
+    time.sleep(2)
+    servo_set_angle(90)
+    time.sleep(2)
