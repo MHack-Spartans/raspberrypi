717ff57e21f01195c23992b045a84fc113b7c732
Austin Evans <austinjevans@austins-mbp.local>
2018-10-13 14:20:35 -0400

    fix

--- pill_serial.py
+++ pill_serial.py
@@ -3,7 +3,7 @@ import serial
 def activate_slot(slot_num):
     ser.write(str(slot_num))
 
-ser = serial.Serial("/dev/ttyUSB0", 9600)
+ser = serial.Serial("/dev/ttyACM0", 9600)
 ser.flushInput()
 
 while True:
