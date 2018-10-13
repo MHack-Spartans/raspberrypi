7ee968ec39981961dddd368e43d0cd1bd7af84a3
Austin Evans <austinjevans@austins-mbp.local>
2018-10-13 14:45:11 -0400

    main

--- pill_serial.py
+++ pill_serial.py
@@ -6,6 +6,7 @@ def activate_slot(slot_num):
 ser = serial.Serial("/dev/ttyACM0", 9600)
 ser.flushInput()
 
-while True:
-    num = input("Slot: ")
-    activate_slot(num)
+if __name__ == "__main__":
+    while True:
+        num = input("Slot: ")
+        activate_slot(num)
